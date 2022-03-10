af642381fcdaad3bfb1b0ae4f52ec94e46f28ac7
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-10 22:07:14 +0300

    Задача 5_4 - DONE)

---
+++ Задача_5_4/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+let rec cifrSum n = 
+    if n = 0 then 0
+    else (n%10) + (cifrSum (n / 10))
+[<EntryPoint>]
+let main argv = 
+    System.Convert.ToInt32(System.Console.ReadLine()) |> cifrSum |> System.Console.WriteLine
+    let z = System.Console.ReadKey()
+    0 
