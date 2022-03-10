f334d8249b089c4a1571b3c09e6e8fbe0e12b53e
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-10 21:24:56 +0300

    Сделала 2 задачу из 5 лабы

---
+++ Задача_5_2/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+let D a b c =Math.Sqrt(b*b-4.0*a*c)
+let x1 a b D = ((-b)+D)/(2.0*a)
+let x2 a b D = ((-b)-D)/(2.0*a)
+
+
+[<EntryPoint>]
+let main argv =
+    let a = System.Convert.ToDouble(System.Console.ReadLine())
+    let b = System.Convert.ToDouble(System.Console.ReadLine())
+    let c = System.Convert.ToDouble(System.Console.ReadLine())
+    let Diskr:float = D a b c 
+    let X1:float= x1 a b Diskr
+    let X2:float= x2 a b Diskr
+    System.Console.WriteLine(X1)
+    System.Console.WriteLine(X2)
+    0 
