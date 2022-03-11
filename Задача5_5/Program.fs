7cb673aecf99c19591311c61f8aab7dbfc5976c4
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-11 16:44:59 +0300

    5_5 - DONE)

---
+++ Задача5_5/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+//хвосттовая рекурсия
+let sumCifr n = 
+    let rec sumCifr1 n curSum = 
+        if n = 0 then curSum
+        else
+            let n1 = n / 10
+            let cifr = n % 10
+            let newSum = curSum + cifr
+            sumCifr1 n1 newSum
+    sumCifr1 n 0
+
+    //рекурсия вниз
+let SUMcifr n =
+    let rec SUMcifr1 acc n =
+        if (n=0) then acc
+        else SUMcifr1 (acc+n%10) (n/10)
+    SUMcifr1 0 n
+
+
+[<EntryPoint>]
+let main argv =
+    let a=System.Convert.ToInt32(System.Console.ReadLine())
+    let o_1=sumCifr a
+    let O_2=SUMcifr a
+    System.Console.WriteLine(o_1)
+    System.Console.WriteLine(O_2)
+    0 
