b2a6d7b8fb64888b76a837ac84208f368b6e0a6e
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-11 19:43:49 +0300

    5_7-DONE)

---
+++ Задача5_7/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec f7 n f init = 
+    if n = 0 then init 
+    else 
+        let cifr = n % 10
+        let n1 = n / 10
+        let acc = f init cifr
+        f7 n1 f acc
+
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine(f7 157 (fun x y -> x * y) 1)
+    0 
