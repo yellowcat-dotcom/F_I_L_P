e4b3f60d3b93ffe57810d81fa19c3b4991e0be1b
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-11 21:25:36 +0300

    5_9)

---
+++ Задача5_9/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let rec f9 n f init predicate = 
+    if n = 0 then init
+    else
+        let cifr = n % 10
+        let n1 = n / 10
+        let acc = f init cifr
+        if predicate cifr then f9 n1 f acc predicate
+        else f9 n1 f init predicate 
+
+[<EntryPoint>]
+let main argv =
+    let a= System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine(f9 a (fun x y -> x + y) 0 (fun x -> x%2=0))
+    0 
