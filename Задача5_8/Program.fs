0e68adfc0cecf7a3f1e8184606b5ba836c805d70
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-11 20:33:31 +0300

    5_8))))

---
+++ Задача5_8/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let rec f7 n f init = 
+    if n = 0 then init 
+    else 
+        let cifr = n % 10
+        let n1 = n / 10
+        let acc = f init cifr
+        f7 n1 f acc
+
+[<EntryPoint>]
+let main argv =
+    let a=System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine(f7 a (fun x y -> x + y) 0)
+    System.Console.WriteLine(f7 a (fun x y -> x * y) 1)
+    System.Console.WriteLine(f7 a (fun x y -> if x > y then x else y) -1)
+    System.Console.WriteLine(f7 a (fun x y -> if x < y then x else y) 10)
+    0 
