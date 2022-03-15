757a9644f825f9b5cb37b6cae54046a02e9e430d
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-15 17:21:35 +0300

    5.12

---
+++ Задача5_12/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let Otvet (k:string) =    
+    match k with 
+    |"F#"|"Prolog" -> "Поздравляю, Вы - подлиза!"
+    | _ -> "Как можно не любить F#!? Чел, ну это кринж, конечно..."
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine("Какой ваш любимый язык программирования?")
+    //Случай суперпозиции
+    (Console.ReadLine>>Otvet>>Console.WriteLine)()
+    System.Console.WriteLine("Какой ваш любимый язык программирования?")
+   // Случай каррирования
+    let F y x z =z(x(y()))
+    F Console.ReadLine Otvet Console.WriteLine
+    0 
