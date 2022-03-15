60787c81bd7b9c67f680ab5f9d5734b4db59927f
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-15 16:21:12 +0300

    5.11

---
+++ Задача5_11/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let Otvet k =    
+    match k with 
+    |1|2 -> System.Console.WriteLine("Поздравляю, Вы - подлиза!")
+    | _ ->System.Console.WriteLine("Как можно не любить F#!? Чел, ну это кринж, конечно...")
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine("Какой ваш любимый язык программирования?
+    1 F#
+    2 Prolog
+    3 C#
+    4 C++")
+    let k=System.Convert.ToInt32(System.Console.ReadLine())
+    Otvet k 
+    0 
