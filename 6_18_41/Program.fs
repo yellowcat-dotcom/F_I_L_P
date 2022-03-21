1d99b1661ee3e8bc68e124408d69eb121c6c7b61
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-21 19:31:39 +0300

    6.18.41

---
+++ 6_18_41/Program.fs
@@ -0,0 +1,18 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое модулей его элементов.
+open System
+
+let rec ob list init f =
+    match list with
+    |[]->init
+    |h::t -> 
+        let new_init = f init h
+        ob t new_init f    
+
+[<EntryPoint>]
+let main argv =
+    let l = Program.readData
+    let sum = ob l 0 (fun x y -> x + Math.Abs(y))
+    let kol = ob l 0 (fun x y -> x+1)
+    let r = sum/kol
+    System.Console.WriteLine("Сумма: {0}    Кол-во:{1}   Среднее арифметическое модулей {2}", sum, kol, r)
+    0 
