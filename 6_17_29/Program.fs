0559839917cf00acd6442491853f0a8ae236cfe6
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-22 20:41:07 +0300

    6_17_29

---
+++ 6_17_29/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let Proverka (a,b) max =
+        if max >a && max<b then true else false
+
+[<EntryPoint>]
+let main argv =
+    let l=Program.readData
+    Console.WriteLine("Введите границы интервала")
+    let interval = (Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()))
+    let max= Program.max l 
+    System.Console.WriteLine(Proverka interval max)
+    0 // return an integer exit code
+   
+    
