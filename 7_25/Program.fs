cfbbb5ccbeec42ae85c82a7fe03af0b9460dfc3c
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-29 22:25:58 +0300

    мне все еще нравится

---
+++ 7_25/Program.fs
@@ -0,0 +1,17 @@
+﻿//1.25 Дан целочисленный массив и интервал a..b. Необходимо найти максимальный из элементов в этом интервале.
+open System
+
+[<EntryPoint>]
+let main argv =
+    let l = Program.readData
+    System.Console.WriteLine("Введите индекс начала интервала")
+    let index_a = Convert.ToInt32(Console.ReadLine())
+    System.Console.WriteLine("Введите индекс конца интервала")
+    let index_b = Convert.ToInt32(Console.ReadLine())
+    let new_l = List.filter (fun x -> x > index_a && x < index_b) l
+    //System.Console.WriteLine("spisok_otvet")
+    //Program.writelist new_l
+    let max_new_spisok = List.max new_l
+    System.Console.WriteLine("otvet: {0}", max_new_spisok)
+
+    0 
