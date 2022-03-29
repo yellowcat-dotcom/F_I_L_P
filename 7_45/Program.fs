d58fe46a1c1ca37d7f8d6dc8fdb8977ec124c4c8
Валентина Гончаренко <valentinagoncarenko975@gmail.com>
2022-03-29 23:31:38 +0300

    агонь пожар легко

---
+++ 7_45/Program.fs
@@ -0,0 +1,15 @@
+﻿//Дан целочисленный массив и интервал a..b. Необходимо найти сумму элементов, значение которых попадает в этот интервал.
+open System
+
+[<EntryPoint>]
+let main argv =
+    let l = Program.readData
+    System.Console.WriteLine("Введите индекс начала интервала")
+    let index_a = Convert.ToInt32(Console.ReadLine())
+    System.Console.WriteLine("Введите индекс конца интервала")
+    let index_b = Convert.ToInt32(Console.ReadLine())
+    let new_l = List.filter (fun x -> x > index_a && x < index_b) l    
+    //Program.writelist new_l    
+    let o = List.sum new_l
+    System.Console.WriteLine("Сумма элементов, входящих в интервал {0}", o)
+    0 
